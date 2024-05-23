fed4265d26151d2beda42eaac1feab5c1dc09da1
Benjamin J. Navarro <benjnavarro28@gmail.com>
2024-05-23 17:49:43 -0500

    Exercise 5 (sumAll) completed and tested

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-    
+const removeFromArray = function(array, ...args) {
+    const newArray = array;
+    args.forEach((arg) => {
+        newArray.filter((el) => el.toLowerCase().includes(arg.toLowerCase()));
+    });
+    return newArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,20 @@
 const sumAll = function(num1, num2) {
-    finalSum = num1;
-    addToSum = num1 + 1;
-
-    while (addToSum < num2) {
-        finalSum += addToSum;
-        addToSum += 1;
+    let largestNum = Math.max(num1, num2);
+    let smallestNum = Math.min(num1, num2);
+    let finalSum = 0;
+    let errorMsg = "ERROR";
+    if (typeof num1 != 'number' || typeof num2 != 'number') {
+        return errorMsg;
+    } else if (smallestNum >= 0 && largestNum >= 0) {
+        for (let i = smallestNum; i <= largestNum; i++) {
+            finalSum += i;
+        }
+        return finalSum;
+    } else {
+        return errorMsg; // Negative numbers cannot be used as inputs.
     }
-    return finalSum;
 };
+sumAll();
 
 // Do not edit below this line
 module.exports = sumAll;
